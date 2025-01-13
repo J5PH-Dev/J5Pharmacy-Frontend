f6d4428da5bbe78c25072f4fad82cfda6523f078
Meixii <iflrix2704@gmail.com>
2025-01-14 04:18:08 +0800

    Migrating Repository - Frontend
    - removing backend folder.

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const prescriptionRoutes = require('./routes/prescription.routes');
-const rfidRoutes = require('./routes/rfid.routes');
-const dashboardRoutes = require('./routes/dashboard.routes');
-
-// Routes
-app.use('/api/prescriptions', prescriptionRoutes);
-app.use('/api/rfid', rfidRoutes);
-app.use('/api/dashboard', dashboardRoutes); 
